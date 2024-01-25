dc598ad920d9529494dd79c66c418868dcb871d6
amirrezabahramy <amirrezabahramy@gmail.com>
2024-01-25 09:51:48 +0330

    Added types for promise version of mutate fn

--- src/types/hooks/useMutateTypes.ts
+++ src/types/hooks/useMutateTypes.ts
@@ -12,8 +12,12 @@ export type TUseMutateProps<TRequestBody, TResponseData> =
   };
 
 export type TMutate<TRequestBody> = (body?: TRequestBody) => void;
+export type TMutateAsync<TRequestBody, TResponseData> = (
+  body?: TRequestBody
+) => Promise<TResponseData>;
 
 export type TUseMutateReturnObject<TRequestBody, TResponseData> =
   TBaseQueryReturnObject<TResponseData> & {
     mutate: TMutate<TRequestBody>;
+    mutateAsync: TMutateAsync<TRequestBody, TResponseData>;
   };
